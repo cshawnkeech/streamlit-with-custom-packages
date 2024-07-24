ddb55eb4bede6e2001ee2da4bfbfcafff9296a0e
C Shawn Keech <CShawnKeech@Gmail.com>
2024-07-24 12:22:48 -0600

    csk: adjust filepath

--- src/constants.py
+++ src/constants.py
@@ -5,6 +5,7 @@ Some constants
 LAT = 37.12809805179428
 LNG = -93.23978460381035
 
-FORECAST_POINT_LOCATION_ENDPOINT = f"https://api.weather.gov/points/{
-    LAT},{LNG}"
+FORECAST_POINT_LOCATION_ENDPOINT = (
+    f"https://api.weather.gov/points/{LAT},{LNG}")
+
 KS_ALERTS_ENDPOINT = "https://api.weather.gov/alerts/active?area=KS"
