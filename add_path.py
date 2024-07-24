b7d1cc6fc709263b177d391c57ad1e61e2655dbb
C Shawn Keech <CShawnKeech@Gmail.com>
2024-07-24 12:36:20 -0600

    csk: refactor

--- add_path.py
+++ add_path.py
@@ -3,4 +3,9 @@ code to add path
 """
 import sys
 
-sys.path.append(".")
+
+def add_dir_to_path(filepath="."):
+    """add filepath to path if not already there"""
+
+    if filepath not in sys.path:
+        sys.path.append(filepath)
--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 Detect file structure for Streamlit app
 discover method for adding custom module to path
 """
-
 import os
 import sys
 import streamlit as st
 
-import add_path
+from add_path import add_dir_to_path
 
 from src.constants import KS_ALERTS_ENDPOINT
+
+add_dir_to_path(os.getcwd())
 
 p = sys.path
 
